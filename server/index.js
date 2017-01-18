d36c21b781baa07f0663aa5dc71811df707ade37
davellaj <jamie.davella@gmail.com>
2017-01-18 02:14:03 -0500

    working get from dictionary, not working put to flashcards

--- server/index.js
+++ server/index.js
@@ -19,8 +19,9 @@ app.use(express.static(process.env.CLIENT_PATH));
 app.use(jsonParser)
 
 // get for logged in users database info
-app.get('/flashCards/:id', (req,res)=>{
-    User.findById(req.params.id)
+// return the userObj that has the users array of words and correctCount
+app.get('/flashCards/:userId', (req,res)=> {
+    User.findById(req.params.userId)
     .then(userObj => {
         return res.status(200).json(userObj)
     })
@@ -29,11 +30,42 @@ app.get('/flashCards/:id', (req,res)=>{
     })
 })
 
+// get dictionary word to display
+//return word object with english and german versions of that one word
+// needs to be passed in params the wordId (which is the key of whatever
+//word the user is on in their array of words) // bad efficiency if server is pinged for every render of new word???
+app.get('/dictionary/:wordId', (req, res)=> {
+    Dictionary.findById(req.params.wordId)
+    .then(wordObj => {
+        return res.status(200).json(wordObj)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+})
+
 //Update a users word and mValue
-// app.put('/flashCards/:id', (req, res) => {
-//     //in body send the wordId req.body.word and the new mValue req.body.mValue
-//     User.findOneAndUpdate({_id: req.params.id}, $set: {req.body.word})
-// })
+app.put('/flashCards/:userId', (req, res) => {
+    // could potentialy do algorithm computation here but for now will assume
+    // it is done on the frontend and frontend sends in body of put request
+    // the wordId and computed mValue
+    // also send me index value of the word
+    let wordId = req.body.word
+    let mValue = req.body.mValue
+    //find logged in user
+    User.findById(req.params.userId)
+    .then(userObj => {
+        for (i=0; i<userObj.length; i++) {
+            
+        }
+        return res.status(200).json(userObj)
+    })
+    .catch(err => {
+        res.status(500).json(err)
+    })
+    //in body send the wordId req.body.word and the new mValue req.body.mValue
+    // User.findOneAndUpdate({_id: req.params.id}, $set: {req.body.word})
+})
 
 
 //post newUser
@@ -48,6 +80,7 @@ app.post('/users', (req, res) => {
                 return word
 
             })
+            console.log(learn)
             return learn
         })
         .then((learn) => {
@@ -69,7 +102,7 @@ app.post('/users', (req, res) => {
 })
 
 // Currently not needed: post to add a flashCard to dictionary
-app.post('/flashCards', (req, res) => {
+app.post('/dictionary', (req, res) => {
     let word = new Dictionary()
     word.english = req.body.english
     word.german = req.body.german
